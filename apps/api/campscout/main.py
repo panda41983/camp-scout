e9ad0015548c623ccd91432ffa701a61b9827628
Mary Laska <marylaska2@gmail.com>
2026-04-26 13:14:03 +0100

    small bug fix
    no .name for PrintLoggerFactory

--- apps/api/campscout/main.py
+++ apps/api/campscout/main.py
@@ -30,7 +30,6 @@ def _configure_logging(environment: str) -> None:
     shared_processors: list[structlog.types.Processor] = [
         structlog.contextvars.merge_contextvars,
         structlog.stdlib.add_log_level,
-        structlog.stdlib.add_logger_name,
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.UnicodeDecoder(),
